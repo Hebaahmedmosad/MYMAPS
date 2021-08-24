a67f1f31f725df4576858be8e153f1e06cf3f61f
Hebaahmedmosad <47794834+Hebaahmedmosad@users.noreply.github.com>
2021-08-24 07:14:14 +0200

    project with map

---
+++ android/app/src/main/kotlin/com/example/demoday4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.iti.demoday4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+import Flutter
+import GoogleMaps
+
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCAteIkUtSZrR8E-8l5uMtLSMGmNK-Xl04")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
